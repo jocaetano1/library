6bb2b434daf67b8eee91153c5555539c93706cfe
jocaetano1 <pogrenyak@hotmail.com>
2021-04-25 03:41:28 +0100

    Redireciomento da rota para a views correcta

--- api/urls.py
+++ api/urls.py
@@ -5,22 +5,5 @@ app_name = 'api'
 
 
 urlpatterns = [
-    path('', views.BookTestAPIView.as_view(), name='list'),
+    path('', views.BookAPIView.as_view(), name='list'),
 ]
-
-"""
-        {% if books %}
-        <h1>All Books</h1>
-        {% for book in books %}
-            <ul>
-                <li>Title: {{ book.title }}</li>
-                <li>Subtitle: {{ book.subtitle }}</li>
-                <li>Author: {{ book.author }}</li>
-                <li>ISBN: {{ book.isbn }}</li>
-            </ul>
-        {% empty %}
-        <p>No books avaliable.</p>
-        {% endfor %}
-        {% endif %}    
-
-"""
